63c0396f74d7ddefd75a02e0963a2b146b0244b4
keerthi-kraft <keerthika.r@kraftpixel.com>
2026-08-19 13:53:28 +0530

    Fix forget password (#4)
    * Forget password UI
    
    * LogInForm changes

--- app/components/LogInForm/LogInForm.tsx
+++ app/components/LogInForm/LogInForm.tsx
@@ -1,32 +1,37 @@
 "use client";
-
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-
 import styles from "./LoginForm.module.scss";
 import logo from "@/public/companyLogo.png";
-import ThemeToggle from "../ThemeToggle/ThemeToggle";
+import ForgotPassword from "@/app/components/ForgetPassword/ForgetPassword";
 
 const LogInForm: React.FC = () => {
   const router = useRouter();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const [showForgotPassword, setShowForgotPassword] =
+    useState(false);
+
+  const handleSubmit = (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
 
-    const username = formData.get("username") as string;
+    const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
     // Temporary frontend login
-    // Backend authentication will be added later
-    if (username && password) {
+    if (email && password) {
       const user = {
-        name: username,
-        email: username,
+        name: email,
+        email: email,
       };
 
-      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem(
+        "user",
+        JSON.stringify(user)
+      );
 
       router.push("/");
     }
@@ -56,17 +61,19 @@ const LogInForm: React.FC = () => {
         className={styles.form}
         onSubmit={handleSubmit}
       >
-        <label htmlFor="username">
-          Username
+        {/* Email */}
+        <label htmlFor="email">
+          Email
         </label>
 
         <input
-          type="text"
-          id="username"
-          name="username"
+          type="email"
+          id="email"
+          name="email"
           required
         />
 
+        {/* Password */}
         <label htmlFor="password">
           Password
         </label>
@@ -78,10 +85,16 @@ const LogInForm: React.FC = () => {
           required
         />
 
-        <p className={styles.passwordText}>
-          forgot password?
-        </p>
+        {/* Forgot Password */}
+        <button
+          type="button"
+          className={styles.passwordText}
+          onClick={() => setShowForgotPassword(true)}
+        >
+          Forgot password?
+        </button>
 
+        {/* Sign In */}
         <button
           type="submit"
           className={styles.submitBtn}
@@ -89,6 +102,7 @@ const LogInForm: React.FC = () => {
           Sign In
         </button>
 
+        {/* Sign Up */}
         <p className={styles.text}>
           Don't have an account?{" "}
           <a
@@ -99,6 +113,13 @@ const LogInForm: React.FC = () => {
           </a>
         </p>
       </form>
+
+      {/* Forgot Password Modal */}
+      {showForgotPassword && (
+        <ForgotPassword
+          onClose={() => setShowForgotPassword(false)}
+        />
+      )}
     </div>
   );
 };
